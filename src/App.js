2734f95513695d682340a91983fd24463f7da2b2
martynas <martynas.pazusis@gmail.com>
2021-01-12 22:11:19 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import LottoGame from './components/lootoGame/lottoGame';
 function App() {
   return (
     <div className="App">
-      <LottoGame maxNum={50} maxBalls={6} />
+      <LottoGame maxNum={50} maxBalls={6} title="Bingo" />
     </div>
   );
 }
